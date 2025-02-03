a3ee6c46270ba0e03b39f8f12833fafcc21de1b3
CJ <teohcjay@gmail.com>
2025-02-03 09:47:50 +0800

    changes

---
+++ app/src/main/java/com/example/assignmentsecond/about_us.java
@@ -0,0 +1,13 @@
+package com.example.assignmentsecond;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class about_us extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
